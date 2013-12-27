a8906a3ac44d00799446cf2c9f25d490401f32ba
7imbrook <timbrook480@gmail.com>
2013-12-26 19:41:04 -0500

    Initial Commit

---
+++ SSE/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SSE
+//
+//  Created by Michael Timbrook on 12/26/13.
+//  Copyright (c) 2013 Michael Timbrook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
